3ed34a20bd03faabfd8719e9e5d6077ceaabd122
amm834 <aungmyatmoe834@gmail.com>
2022-12-12 23:27:14 +0630

    fix: add favicon for SEO

--- app/head.jsx
+++ app/head.jsx
@@ -41,7 +41,8 @@ export default async function Head() {
             <title>Aung Myat Moe - Software Engineer, Founder and Open Source Enthusiast</title>
             <meta content="width=device-width, initial-scale=1" name="viewport"/>
             <meta name="description" content="Aung Myat Moe - Software Engineer, Founder and Open Source Enthusiast"/>
-            <meta name="robots" content="all" />
+            <meta name="robots" content="all"/>
+
 
             <Script dangerouslySetInnerHTML={{__html: darkModeToggleScript}}/>
             <link rel="icon" href="/favicon.ico"/>
@@ -55,6 +56,11 @@ export default async function Head() {
                 type="application/feed+json"
                 href={`${process.env.NEXT_PUBLIC_SITE_URL}/rss/feed.json`}
             />
+
+            <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+            <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+            <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+            <link rel="manifest" href="/site.webmanifest"/>
         </>
     )
 }
