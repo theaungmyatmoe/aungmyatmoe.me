cfd2792cfb9eafcf0d97a4684da948069eaa2e8c
Aung Myat Moe <aungmyatmoe834@gmail.com>
2023-08-11 15:33:56 +0630

    feat: add cssnano

--- postcss.config.js
+++ postcss.config.js
@@ -2,8 +2,9 @@ module.exports = {
     plugins: {
         tailwindcss: {},
         'postcss-focus-visible': {
-            replaceWith: '[data-focus-visible-added]',
+            replaceWith: '[data-focus-visible-added]'
         },
         autoprefixer: {},
-    },
-}
+        ...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {})
+    }
+};
