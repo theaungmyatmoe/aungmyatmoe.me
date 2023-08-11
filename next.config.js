50e2c5ea6299182e6d129e8a1d48f3acfa32d33d
Aung Myat Moe <aungmyatmoe834@gmail.com>
2023-08-11 14:02:51 +0630

    chore: add rehype

--- next.config.js
+++ next.config.js
@@ -3,5 +3,19 @@ module.exports = {
     reactStrictMode: true,
     experimental: {
         appDir: true,
+    },
+    async redirects() {
+        return [
+            {
+                source: '/home',
+                destination: '/',
+                permanent: true,
+            },
+            {
+                source: '/admin',
+                destination: '/admin/index.html',
+                permanent: true,
+            }
+        ]
     }
 }
